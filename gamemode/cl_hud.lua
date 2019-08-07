688927f6069016520b374d3c77e43b09c12604ab
Sydney McKinley <rightowner24@gmail.com>
2019-08-07 18:51:07 -0400

    Basic HUD

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,1 +1,27 @@
 print("cl_hud.lua LOADED")
+
+local HideHud = {
+    ["CHudHealth"] = true,
+    ["CHudAmmo"] = true,
+    ["CHudCrosshair"] = true,
+    ["CHudSecondaryAmmo"] = true,
+    ["CHudBattery"] = true,
+}
+
+local function HUDShouldDraw(name)
+    if ( HideHud[name] ) then
+        return false 
+    end
+end
+
+hook.Add("HUDShouldDraw", "HUDShouldDraw", HUDShouldDraw)
+
+function PaintHud() 
+    local hp = LocalPlayer():Health() / 100
+    draw.RoundedBox(1, 10, ScrH() - 50, 200, 40, Color (0, 0, 0, 200))
+    draw.RoundedBox(1, 14, ScrH() - 46, 192 * hp, 32, Color (200, 0, 0, 200))
+
+    draw.DrawText ( tostring(LocalPlayer():Health()), "Default", 105, ScrH() - 35, color_white, 0 )
+end 
+
+hook.Add("HUDPaint", "PaintHud", PaintHud)
